153ec2a6dca6f31e5a36d1e78ab4756402c75e48
128na <simutrans.128na@gmail.com>
2021-09-11 23:04:26 +0900

    ランダム置換モードを追加

--- dist/src/replaceSpecialColor.js
+++ dist/src/replaceSpecialColor.js
@@ -7,7 +7,7 @@ const specialColors_1 = __importDefault(require("./specialColors"));
 /**
  * replace special color to (r+1,g+1,b+1)
  */
-function replaceSpecialColor(canvas, specialColors = specialColors_1.default) {
+function replaceSpecialColor(canvas, specialColors = specialColors_1.default, random = false) {
     const ctx = canvas.getContext('2d');
     const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     const data = imageData.data;
@@ -15,9 +15,9 @@ function replaceSpecialColor(canvas, specialColors = specialColors_1.default) {
         const color = [data[i], data[i + 1], data[i + 2]].join(',');
         if (specialColors.includes(color)) {
             [data[i], data[i + 1], data[i + 2]] = [
-                Math.min(255, data[i] + 1),
-                Math.min(255, data[i + 1] + 1),
-                Math.min(255, data[i + 2] + 1),
+                Math.min(255, data[i] + (random ? 1 - Math.random() * 2 : 1)),
+                Math.min(255, data[i + 1] + (random ? 1 - Math.random() * 2 : 1)),
+                Math.min(255, data[i + 2] + (random ? 1 - Math.random() * 2 : 1)),
             ];
         }
     }
